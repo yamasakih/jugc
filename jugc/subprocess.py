5d0c4134f708265b7c4c81a075becd8b3f6c3ac2
yamasakih <yamasaki.phone@gmail.com>
2019-07-22 07:16:25 +0900

    Create calculate_descriptors

---
+++ jugc/subprocess.py
@@ -0,0 +1,22 @@
+import os
+from typing import List
+from typing import Union
+
+from jug import bvalue
+from jug import TaskGenerator
+import pandas as pd
+from rdkit import Chem
+from rdkit import RDConfig
+from rdkit.Chem.rdchem import Mol
+
+from jugc import calculate_descriptors
+
+calculate_descriptors = TaskGenerator(calculate_descriptors)
+
+input_file = os.path.join(RDConfig.RDDataDir, 'NCI', 'first_200.props.sdf')
+supp = Chem.SDMolSupplier(input_file)
+mols: List[Mol] = [mol for mol in supp]
+descs = [calculate_descriptors(mol) for mol in mols]
+
+descs = pd.concat(bvalue(descs))
+descs.to_csv('descs.csv', index=False)
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,14 +1,35 @@
 import pkg_resources
 
+import pandas as pd
 import pytest
+from rdkit.Chem import MolFromSmiles
 
 import jugc
+from jugc import calculate_descriptors
 
 
 def test_version() -> None:
     expect = pkg_resources.get_distribution('jugc').version
     actual = jugc.__version__
     assert expect == actual
+
+
+def test_calculate_descriptors() -> None:
+    mol = MolFromSmiles('c1ccccc1')
+
+    descs = calculate_descriptors(mol, names=['MolWt'])
+
+    actual = descs
+    expect = pd.Series
+    assert isinstance(actual, expect)
+
+    actual = list(descs.index)
+    expect = ['MolWt']
+    assert actual == expect
+
+    actual = descs.MolWt
+    expect = 78.114
+    assert pytest.approx(actual, 0.001) == expect
 
 
 if __name__ == '__main__':
